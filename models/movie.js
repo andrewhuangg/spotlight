12040f2bcfd6dfd7b0b3ea51f8d28062af53116b
Andrew <drewhuangg@gmail.com>
2021-07-15 14:35:37 -0700

    movie model

---
+++ models/movie.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+
+const MovieSchema = new mongoose.Schema(
+  {
+    title: { type: String, required: true, unique: true },
+    description: { String },
+    imageHero: { String },
+    imageTitle: { String },
+    imagePin: { String },
+    trailer: { String },
+    video: { String },
+    year: { String },
+    limit: { Number },
+    genre: { String },
+    isSeries: { type: Boolean, default: false },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model('Movie', MovieSchema);
